2be82926ec493e7b70309435f0e902f21cc53174
Eunice Do <eunicedx@gmail.com>
2016-02-02 16:02:34 -0500

    Add post state

--- app.js
+++ app.js
@@ -1,28 +1,42 @@
 angular.module('readIt', ['ui.router'])
+
+.run(($rootScope) => {
+  $rootScope.$on("$stateChangeError", console.log.bind(console));
+})
+
 .config([
   '$stateProvider',
   '$urlRouterProvider',
-  function($stateProvider, $urlRouterProvider){
-    $stateProvider.state('home', {
-      url: '/home',
-      templateURL: '/home.html',
-      controller: 'MainCtrl'
-    });
+  function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateURL: '/home.html',
+        controller: 'MainCtrl'
+      })
+      .state('posts', {
+        url: '/posts/{id}',
+        templateUrl: '/posts.html',
+        controller: 'PostsCtrl'
+      });
     $urlRouterProvider.otherwise('home');
-  }])
-.factory('posts', [function(){
-  var o = {
-    posts: []
-  };
-  return o;
-}])
+  }
+])
+
+.factory('posts', [
+  function() {
+    var o = { posts: [ ] };
+    return o;
+  }
+])
+
 .controller('MainCtrl', [
   '$scope', 
   'posts',
-  function($scope, posts){
+  function($scope, posts) {
     $scope.posts = posts.posts;
 
-    $scope.addPost = function(){
+    $scope.addPost = function() {
       if (!$scope.title || $scope.title === '') { return; }
       $scope.posts.push({
         title: $scope.title, 
