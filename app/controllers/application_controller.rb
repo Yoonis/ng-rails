cfa9545cd96959312dff5ba88c2b914b9a84fe24
Eunice Do <eunicedx@gmail.com>
2016-02-16 23:59:04 -0500

    Add devise ui-view and proper sign up/in/out links.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
+  respond_to :html, :json
+
   def angular
     render 'layouts/application'
   end
