c66bdfba244a4813faa6b7405cef36d97c4eadb1
Eunice Do <eunicedx@gmail.com>
2016-03-22 20:58:03 -0400

    Add notes on engine characteristics

--- vendor/engines/blogger/test/dummy/config/routes.rb
+++ vendor/engines/blogger/test/dummy/config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-
+  # mount engine in dummy Rails app for engine testing purposes. Engine is only accessible to dummy app via "/blogger" path.
   mount Blogger::Engine => "/blogger"
 end
