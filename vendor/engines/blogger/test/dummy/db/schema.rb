b78aa9f83484d02ba39f957eff4ca1c02974f542
Eunice Do <eunicedx@gmail.com>
2016-03-23 15:29:48 -0400

    Add comments model and migration

--- vendor/engines/blogger/test/dummy/db/schema.rb
+++ vendor/engines/blogger/test/dummy/db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160323020036) do
+ActiveRecord::Schema.define(version: 20160323160702) do
 
   create_table "blogger_articles", force: :cascade do |t|
     t.string   "title"
+    t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "blogger_comments", force: :cascade do |t|
+    t.integer  "article_id"
     t.text     "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
