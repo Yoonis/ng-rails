cdab44c7c403e56a5e30226edadfaf5a86199772
Eunice Do <eunicedx@gmail.com>
2016-03-24 15:16:38 -0400

    Copy engine migrations over to ng-rails db

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160214222513) do
+ActiveRecord::Schema.define(version: 20160324190536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blogger_articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "blogger_comments", force: :cascade do |t|
+    t.integer  "article_id"
+    t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.string   "body"
